8bf0802ab31a97c2bb419d79dab2b46582974471
HamsterStudent <hwanyamyu@gmail.com>
2024-06-04 12:10:11 +0900

    Feat : OAuth redirect 링크 설정

--- src/app/(route)/login/google/auth/page.tsx
+++ src/app/(route)/login/google/auth/page.tsx
@@ -1,10 +1,29 @@
+"use server"
 import React from "react";
 
-export default function Auth() {
-  const getAccessToken = async () => {
-    const parsedHash = new URLSearchParams(window.location.hash.substring(1));
-    const accessToken = parsedHash.get("access_token");
-    // const { data } = await Api.post("oauth/google", { accessToken });
+const Auth = ({ serverData } : any) => {
+  return <div>get data : {serverData}</div>;
+};
+
+export async function getData() {
+   const parsedHash = new URLSearchParams(window.location.hash.substring(1));
+   const accessToken = parsedHash.get("access_token");
+
+  const data = await fetch("api 주소/oauth/google", {
+    method: "POST",
+    next: { revalidate: 3600 },
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ accessToken }),
+  });
+  const serverData = await data.json();
+
+  return {
+    props: {
+      serverData,
+    },
   };
-  return <div>Auth</div>;
 }
+
+export default Auth;
